470c1441116bcb62f38588b20418fa7a75d416ea
AngMingZe <92706642+AngMingZe@users.noreply.github.com>
2022-04-19 19:37:25 +0800

    Update Question2.java
    Code for question 2

--- Question2.java
+++ Question2.java
@@ -10,7 +10,21 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
      
+    public static void main(String args[]) { 
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter height: ");
+    
     double height = in.nextDouble();
+    System.out.println("Your height is: " + height);
+
+    System.out.print("Enter weight: ");
+    double wei = in.nextDouble();
+    System.out.println("Your weight is: " + wei);
+
+    double bmi = wei/(double) Math.pow(height/100, 2);
+    double bmiRounded2 = (double) Math.round(bmi * 100) / 100;
+    
+    System.out.println("Your bmi is: " + bmiRounded2);
   }
 }
+
